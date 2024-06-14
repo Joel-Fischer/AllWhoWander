6b320f3079bb62847aaa28c1dbc6a66ca6efd4b2
Joel Fischer <Fischer.Joel.B@gmail.com>
2024-06-13 21:31:44 -0600

    fixed auth

--- public/js/register.js
+++ public/js/register.js
@@ -1,31 +1,29 @@
 const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    console.log("hello world");
-
-    // const name = document.querySelector('#name').value.trim();
-    // const email = document.querySelector('#email').value.trim();
-    // const password = document.querySelector('#password').value.trim();
-    // const confirmPassword = document.querySelector('#confirm_password').value.trim();
+    const name = document.querySelector('#name').value.trim();
+    const email = document.querySelector('#email').value.trim();
+    const password = document.querySelector('#password').value.trim();
+    const confirmPassword = document.querySelector('#confirm_password').value.trim();
 
   
-    // if (name && email && password && confirmPassword) {
-    //     if(password === confirmPassword){
-    //         const response = await fetch('/api/users', {
-    //             method: 'POST',
-    //             body: JSON.stringify({ name, email, password }),
-    //             headers: { 'Content-Type': 'application/json' },
-    //         });
+    if (name && email && password && confirmPassword) {
+        if(password === confirmPassword){
+            const response = await fetch('/api/users', {
+                method: 'POST',
+                body: JSON.stringify({ name, email, password }),
+                headers: { 'Content-Type': 'application/json' },
+            });
           
-    //         if (response.ok) {
-    //             document.location.replace('/login');
-    //         } else {
-    //             alert(response.statusText);
-    //         }
-    //     } else {
-    //         alert("Password and Confirm Password do not match.");
-    //     }
-    // }
+            if (response.ok) {
+                document.location.replace('/login');
+            } else {
+                alert(response.statusText);
+            }
+        } else {
+            alert("Password and Confirm Password do not match.");
+        }
+    }
   };
 
-  document.getElementById('register-btn').addEventListener('click', signupFormHandler);
+  document.getElementById('register').addEventListener('click', signupFormHandler);
